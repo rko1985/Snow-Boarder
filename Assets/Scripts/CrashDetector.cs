f560a6308eaaf5656a2f87e09caddad19cf54bc8
Russell Oyama <russelloyama@gmail.com>
2021-12-22 09:24:13 -1000

    delay when level loads

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -5,11 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other)
+
+    [SerializeField] float loadDelay = 0.5f;
+
+    void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Ground")
         {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", loadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
