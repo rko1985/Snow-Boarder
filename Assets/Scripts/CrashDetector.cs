747ac77f876a0d1e2136db60a0955be7ac6b3ae0
Russell Oyama <russelloyama@gmail.com>
2021-12-24 08:25:59 -1000

    triggering particle effects with code

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -7,11 +7,13 @@ public class CrashDetector : MonoBehaviour
 {
 
     [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene", loadDelay);
         }
     }
